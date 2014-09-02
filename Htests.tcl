530ab6db08561ec579651cd812b3cee1b2525e02
colin <colin>
2014-09-02 05:40:58 +0000

    * added Htest code for testing timeouts

--- Htests.tcl
+++ Htests.tcl
@@ -13,6 +13,7 @@ Debug off httpdtxlow
 Debug off entity
 Debug define cache
 Debug define cookies
+Debug define httpdchan
 
 interp bgerror {} BGERROR
 proc BGERROR {args} {
@@ -79,25 +80,78 @@ set phase 0
 after 0 {::apply {{} {
     puts stderr "Phase:$::phase Simple Tests"
     
-    test simple-GET {perform a simple GET which returns some html} -setup {
-	set ::listener [H listen process [list ::apply {{r} {
-	    H Ok $r content-type text/html <p>Moop</p>
-	}}] {*}$::defaults $::port]
-    } -body {
-	set token [::http::geturl http://localhost:$::port/ -timeout 100]
-	
-	::http::wait $token
-	
-	test_dict $token {
-	    -code 200
-	    connection close
-	    content-type {text/html; charset=utf-8}
-	    server {H *}
-	    vary accept-encoding
-	    content-length 11
-	} 
-    } -cleanup {
-	chan close $::listener
+    if {1} {
+	test simple-GET {perform a simple GET which returns some html} -setup {
+	    set ::listener [H listen process [list ::apply {{r} {
+		H Ok $r content-type text/html <p>Moop</p>
+	    }}] {*}$::defaults $::port]
+	} -body {
+	    set token [::http::geturl http://localhost:$::port/ -timeout 100]
+	    
+	    ::http::wait $token
+	    
+	    test_dict $token {
+		-code 200
+		connection close
+		content-type {text/html; charset=utf-8}
+		server {H *}
+		vary accept-encoding
+		content-length 11
+	    } 
+	} -cleanup {
+	    chan close $::listener
+	}
+    }
+
+
+    if {1} {
+	test simple-GET-with-timeout {test 1 second timeout} -setup {
+	    set ::listener [H listen rx {timeout {"" 1} ondisconnect {::apply {{coro vars} {
+		upvar #1 close close
+		set ::rxtimeout $close
+	    }}}} process [list ::apply {{r} {
+		H Ok $r content-type text/html <p>Moop</p>
+	    }}] {*}$::defaults $::port]
+	} -body {
+	    try {
+		set waiter [::socket localhost $::port]	;# now do nothing
+		vwait ::rxtimeout
+		close $waiter
+		set ::rxtimeout
+	    } on error {e eo} {
+		puts stderr "ERR: $e ($eo)"
+	    }
+	} -cleanup {
+	    chan close $::listener
+	} -result {TIMEOUT Header}
+    }
+
+    if {1} {
+	test simple-GET-with-timeout2 {test 1 second entity timeout} -setup {
+	    set ::listener [H listen rx {timeout {"" 1} ondisconnect {::apply {{coro vars} {
+		upvar #1 close close
+		set ::rxtimeout $close
+	    }}}} process [list ::apply {{r} {
+		H Ok $r content-type text/html <p>Moop</p>
+	    }}] {*}$::defaults $::port]
+	} -body {
+	    try {
+		set waiter [::socket localhost $::port]	;# now do nothing
+		chan configure $waiter -translation crlf
+		puts $waiter "GET / HTTP/1.1"
+		puts $waiter "content-length: 100"
+		puts $waiter ""
+
+		# now do nothing
+		vwait ::rxtimeout
+		close $waiter
+		set ::rxtimeout
+	    } on error {e eo} {
+		puts stderr "ERR: $e ($eo)"
+	    }
+	} -cleanup {
+	    chan close $::listener
+	} -result {TIMEOUT Header}
     }
 
     test simple-BINARY {send 1k of random bytes to the server, which echoes it back unchanged, compare received with sent data} -setup {
@@ -119,10 +173,12 @@ after 0 {::apply {{} {
 	::http::wait $token
 	#puts stderr "META:[::http::meta $token]"
 	if {[::http::data $token] eq $::body} {
-	    return ""
+	    set result ""
 	} else {
-	    return "'[binary encode hex $::body]' != '[binary encode hex [::http::data $token]]'"
+	    set result "'[binary encode hex $::body]' != '[binary encode hex [::http::data $token]]'"
 	}
+	::http::cleanup $token
+	return $result
     } -cleanup {
 	chan close $::listener
     }
@@ -164,6 +220,7 @@ after 0 {::apply {{} {
 }}}
 
 vwait phase	;# wait for this testing phase to finish
+puts "Phase $phase Open Chans [llength [chan names]]: [chan names]"
 
 # the tests need to be in event space
 after 0 {::apply {{} {
@@ -208,12 +265,13 @@ after 0 {::apply {{} {
 
 vwait phase	;# wait for this testing phase to finish
 puts stderr "Elapsed time in [expr {$phase - 1}]: [expr {[clock microseconds] - $::start_time}]uS"
+puts "Phase $phase Open Chans [llength [chan names]]: [chan names]"
 
 # the tests need to be in event space
 after 0 {::apply {{} {
     puts stderr Phase:$::phase
 
-    set ::listener [H listen timeout 0 process [list ::apply {{r} {
+    set ::listener [H listen timeout {} process [list ::apply {{r} {
 	# this server snippet pauses randomly, then returns the uri trailing element as an entity
 	#puts stderr "SETTING[info coroutine]/[llength [chan names]] ($r)"
 	after [expr {int(10000 * rand())}] [info coroutine]
